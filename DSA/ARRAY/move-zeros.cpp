44422a0bde301dd8982a2b73dc2f5ed99400155d
Prakarti Goel <prakartigoel24@gmail.com>
2021-09-16 15:24:14 +0530

    Create move-zeros.cpp

---
+++ DSA/ARRAY/move-zeros.cpp
@@ -0,0 +1,23 @@
+
+
+//Link: https://leetcode.com/problems/move-zeroes/
+
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+     
+for (int lastNonZeroele = 0, cur = 0; cur < nums.size(); cur++) {
+    
+    
+        if (nums[cur] != 0) 
+        {
+            swap(nums[lastNonZeroele++], nums[cur]);
+        }
+    
+        
+}  
+        
+        
+    }
+};
