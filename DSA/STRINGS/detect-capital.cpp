9f8b31a87eee5264b726ebd2a854a9a87a1d6af4
Prakarti Goel <prakartigoel24@gmail.com>
2021-09-24 15:43:24 +0530

    Update detect-capital.cpp

--- DSA/STRINGS/detect-capital.cpp
+++ DSA/STRINGS/detect-capital.cpp
@@ -1,5 +1,32 @@
 //LINK : https://leetcode.com/problems/detect-capital/
 
+//solution- 1
+
+
+class Solution {
+public:
+    bool detectCapitalUse(string w) {
+        
+        int n = w.size(); int upperletters=0;
+       for(int i=0 ;i<n;i++)
+       {
+           if(w[i]>= 'A' and w[i]<='Z')
+               upperletters++;
+       }
+        
+        
+        if(upperletters==1)
+        {
+            return isupper(w[0]);
+        }
+        
+        return (upperletters==0 || upperletters==n);
+     
+   }
+};
+
+//solution-2
+
 class Solution {
 public:
     bool detectCapitalUse(string w) {
