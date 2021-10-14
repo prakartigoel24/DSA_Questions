6816e2f325ac3ef5f5faa081771d4d6a7df6882c
Prakarti Goel <prakartigoel24@gmail.com>
2021-10-14 16:43:31 +0530

    Update Delete Node in a LL.cpp

--- DSA/LinkedList/Delete Node in a LL.cpp
+++ DSA/LinkedList/Delete Node in a LL.cpp
@@ -1,1 +1,35 @@
+//LINK : https://leetcode.com/problems/delete-node-in-a-linked-list/
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+//BRUTE FORCE 
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        
+        ListNode *temp =node->next;
+        
+        node->val=temp->val;
+        node->next=temp->next;
+    
+    }
+};
+
+
+//OPTIMISED SOLUTION
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        
+        node->val=node->next->val;
+        node->next=node->next->next;
+    }
+};
