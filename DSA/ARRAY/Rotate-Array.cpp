c30b7f153bd44ea236eedc5663ca673c1f168849
Prakarti Goel <prakartigoel24@gmail.com>
2021-10-26 22:31:36 +0530

    Create Rotate-Array.cpp

---
+++ DSA/ARRAY/Rotate-Array.cpp
@@ -0,0 +1,36 @@
+//LINK : https://leetcode.com/problems/rotate-array/
+
+//using extra space
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        
+    int i=1;
+    int n=nums.size() ; deque<int> v;
+    if(n==k or k==0) return;
+        
+     while(k>n)
+        k=k-n;
+        
+    int l= n-k;
+        
+    while(k--) 
+    { 
+        int ele=nums[n-i];
+         v.push_front(ele); i++;
+    }
+        
+    i=0;
+    while(l--)
+    {
+        v.push_back(nums[i]); i++;
+    }
+    
+i=0;
+for(auto it:v)
+    nums[i++]=it;
+     
+        
+return;
+    }
+};
