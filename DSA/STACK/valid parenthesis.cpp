08c9d39f0feedc6d895162c5154bf5c827cf4976
Prakarti Goel <prakartigoel24@gmail.com>
2021-11-11 20:42:04 +0530

    Create valid parenthesis.cpp

---
+++ DSA/STACK/valid parenthesis.cpp
@@ -0,0 +1,38 @@
+//LINK :   https://leetcode.com/problems/valid-parentheses/
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        
+        if (s.length()==1)
+        {
+            return false;
+        }
+        
+        for(char c: s)
+        {
+            switch(c){
+                    
+                case '(':
+                case '[':
+                case '{': st.push(c); break;
+                    
+                case ')':
+                    if(st.empty() || st.top()!='(' )  return false;
+                     else st.pop(); break;
+                case ']':
+                    if(st.empty()||st.top()!='[') return false;
+                    else st.pop(); break;;
+                case '}':
+                    if(  st.empty() || st.top()!='{' ) return false;
+                    else st.pop(); break;
+  
+            }
+
+            
+        }
+          return st.empty(); 
+            
+    }
+};
