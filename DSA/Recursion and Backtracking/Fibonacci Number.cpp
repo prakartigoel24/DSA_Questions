5deb8170a152400097d257b1a5d42088ffd298e6
Prakarti Goel <prakartigoel24@gmail.com>
2021-11-29 19:26:07 +0530

    Create Fibonacci Number.cpp

---
+++ DSA/Recursion and Backtracking/Fibonacci Number.cpp
@@ -0,0 +1,14 @@
+//LINK : https://leetcode.com/problems/fibonacci-number/
+
+class Solution {
+public:
+    int fib(int n) {
+        
+        if(n==0 or n==1)
+            return n;
+        
+        return fib(n-1)+fib(n-2);
+           
+        
+    }
+};
