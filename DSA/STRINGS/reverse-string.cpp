a678f1b20312d2db2c16780d8b3e8dcd6ff6d7c6
Prakarti Goel <prakartigoel24@gmail.com>
2021-12-19 15:53:29 +0530

    Update reverse-string.cpp

--- DSA/STRINGS/reverse-string.cpp
+++ DSA/STRINGS/reverse-string.cpp
@@ -15,3 +15,29 @@ public:
         return ;
     }
 };
+
+
+//using recursion - method 1
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        
+        if(s.size()<=1)
+        {
+            return;
+        }
+
+          char ch = s[0];
+          s.erase(s.begin());
+            reverseString(s);
+            s.push_back(ch);
+        return;
+        
+    }
+};
+
+
+
+
+
