9410d82552147c86567b015de60f5d5c6f230570
Prakarti Goel <prakartigoel24@gmail.com>
2021-12-19 18:40:32 +0530

    Update reverse LL .cpp

--- DSA/LinkedList/reverse LL .cpp
+++ DSA/LinkedList/reverse LL .cpp
@@ -31,3 +31,28 @@ public:
         return prev;
     }
 };
+//RECURSIVE SOLUTION
+
+class Solution {
+public:
+    ListNode* newHead;
+    ListNode* reverseList(ListNode* head) {
+        
+        if(head==NULL)
+            return head;
+        if(head->next==NULL)
+        {
+            newHead=head;
+            return newHead;
+        }
+        
+        reverseList(head->next);
+        
+        ListNode* temp=head->next;
+        temp->next=head;
+        head->next=NULL;
+       
+        return newHead;
+        
+    }
+};
