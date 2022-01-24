a7e3fee168227aed04362da925078901a0a3c94d
Prakarti Goel <prakartigoel24@gmail.com>
2022-01-24 21:11:31 +0530

    Update perfect-squares.cpp

--- DSA/DP/perfect-squares.cpp
+++ DSA/DP/perfect-squares.cpp
@@ -25,3 +25,31 @@ public:
         return dp[n];
     }
 };
+
+//Memoisation
+class Solution {
+int dp[10005];
+public:
+    int Squares(int n) {
+        
+        if(n<=0) return 0;
+        if(n<=3) return n;
+        
+        if(dp[n]!=-1) return dp[n];
+            
+        int m=INT_MAX;
+            for(int j=1;j*j <=n ;j++)
+            {
+                m= min(m,Squares(n - (j*j)));
+            }
+           
+      return dp[n]=m+1;
+       
+    }
+    
+    int numSquares(int n) {
+        
+        memset(dp,INT_MAX,sizeof(dp));
+       return Squares(n);
+    }
+};
