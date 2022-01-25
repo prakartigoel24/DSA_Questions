ed2d32ffec98e8777333cfc962bdc319930aed1e
Prakarti Goel <prakartigoel24@gmail.com>
2022-01-25 22:20:49 +0530

    Create Pascal's-Triangle.cpp

---
+++ DSA/DP/Pascal's-Triangle.cpp
@@ -0,0 +1,46 @@
+//LINK : https://leetcode.com/problems/pascals-triangle/
+
+//MEMOISATION
+class Solution {
+public:
+    vector<vector<int>> generate2(int numRows,vector<vector<int>> &ans)
+    {
+         vector<vector<int>> dp(numRows,vector<int>(numRows,1));
+        
+        if(numRows==1)
+        {    
+            return ans= dp;
+        }
+        if(!ans.empty()) return ans;
+        
+        dp=generate2(numRows-1,ans);
+        vector<int> newTemp(numRows,0);
+        
+        for(int i=0;i<numRows;i++)
+        {
+            if(i==0 or i==numRows-1)
+            {
+                newTemp[i]=1;
+            }
+            else
+            {
+                newTemp[i]= dp[numRows-2][i]+dp[numRows-2][i-1];
+            }
+        }
+            
+        dp.push_back(newTemp);
+        
+        return ans=dp;
+            
+    }
+
+vector<vector<int>> generate(int numRows) {
+        
+        vector<vector<int>> ans;
+        
+      return generate2(numRows,ans);
+    }
+};
+
+//RECURSION
+
