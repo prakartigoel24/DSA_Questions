1c70f48b220e646b4ba2773a4e58fd94dbdd076f
Prakarti Goel <prakartigoel24@gmail.com>
2022-01-28 20:57:01 +0530

    Update 2 Keys Keyboard.cpp

--- DSA/DP/2 Keys Keyboard.cpp
+++ DSA/DP/2 Keys Keyboard.cpp
@@ -26,3 +26,35 @@ int minSteps(int n) {
         
     }
 };
+
+//MEMOISATION SOLUTION
+class Solution {
+int dp[1010][1010];
+public:
+int steps(int reqA ,int screenA,int clipA)
+    {
+    
+        if(screenA==reqA or reqA==1)  return dp[screenA][clipA]= 0;
+        else if(reqA==2) return dp[screenA][clipA]= 2;
+        else if(screenA > reqA) return 10000;
+    
+    if(dp[screenA][clipA]!=-1) return dp[screenA][clipA];
+        
+        long copy=INT_MAX, paste=INT_MAX;
+        
+        if(screenA!=clipA)
+        copy= 1 + (steps(reqA , screenA , screenA));
+        if(clipA > 0)
+        paste= 1 +(steps(reqA, screenA+clipA,clipA));
+        
+        return dp[screenA][clipA]= min(copy,paste);
+
+    }
+    
+int minSteps(int n) {
+        
+    memset(dp,-1,sizeof(dp));
+        return steps(n,1,0);
+        
+    }
+};
