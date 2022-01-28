b698e92b2af83f6808fba185957da21645dbc6b9
Prakarti Goel <prakartigoel24@gmail.com>
2022-01-28 18:39:47 +0530

    Update Triangle.cpp

--- DSA/DP/Triangle.cpp
+++ DSA/DP/Triangle.cpp
@@ -1,6 +1,25 @@
 //LINK : https://leetcode.com/problems/triangle/
 
 //SPACE OPTIMISED SOLUTION - O(1)
+class Solution {
+public:
+   int minimumTotal(vector<vector<int>>& triangle) {
+        
+        int r=triangle.size();
+        
+        if(r==1) return triangle[0][0];
+ 
+        for(int i=r-2;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                 triangle[i][j]=triangle[i][j]+min(triangle[i+1][j],triangle[i+1][j+1]);
+            }
+        }
+        
+        return triangle[0][0];
+    }
+};
 
 //TABULATION - O(r*r)
 class Solution {
