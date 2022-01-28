930e320ca429cf30a9f345e26239f8873cbddfcc
Prakarti Goel <prakartigoel24@gmail.com>
2022-01-28 18:35:46 +0530

    Create Triangle.cpp

---
+++ DSA/DP/Triangle.cpp
@@ -0,0 +1,34 @@
+//LINK : https://leetcode.com/problems/triangle/
+
+//TABULATION
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        
+        int r=triangle.size();
+        
+        if(r==1) return triangle[0][0];
+        
+        int dp[r][r];
+        memset(dp,INT_MAX,sizeof(dp));
+        
+        
+        for(int i=r-1;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                if(i==r-1)
+                {
+                    dp[i][j]=triangle[i][j];
+                }
+                else
+                {
+                    dp[i][j]=triangle[i][j]+min(dp[i+1][j],dp[i+1][j+1]);
+                }
+            }
+        }
+        
+        return dp[0][0];
+        
+    }
+};
