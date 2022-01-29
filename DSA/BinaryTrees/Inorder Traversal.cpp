a908d9449951e5dc131424acf08f05da4476e559
Prakarti Goel <prakartigoel24@gmail.com>
2022-01-29 23:18:25 +0530

    Update Inorder Traversal.cpp

--- DSA/BinaryTrees/Inorder Traversal.cpp
+++ DSA/BinaryTrees/Inorder Traversal.cpp
@@ -1,3 +1,30 @@
 //LINK : https://leetcode.com/problems/binary-tree-inorder-traversal/
 
 //Recursive approach
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+ vector<int> v;    
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        
+        if(root!=NULL)
+        {
+            inorderTraversal(root->left);
+            v.push_back(root->val);
+            inorderTraversal(root->right);
+  
+        }
+        
+        return v;
+    }
+};
