f0eb6441bf43c9ffde03b6bf8ce4217a2659eec2
Prakarti Goel <prakartigoel24@gmail.com>
2022-01-30 21:04:52 +0530

    Update Target Sum.cpp

--- DSA/DP/Target Sum.cpp
+++ DSA/DP/Target Sum.cpp
@@ -1,32 +1,26 @@
 //LINK : https://leetcode.com/problems/target-sum/
 
+//MEMOISATION 
+
 //RECURSIVE APPROACH
 class Solution {
-int ans=0;
 public:
-    int solve(vector<int>& nums, int i, long long  sum, int tar)
+    int solve(vector<int>& nums, int i, int sum, int tar)
     {
-        int n = nums.size();
-       
+        int n =nums.size();
+      
         if(i==n)
         {
-            if(sum==tar) 
-            {
-                ans++;
-            }
+            if(sum==tar) return 1;
+            else return 0;
         }
         
-        else
-        {
-            solve(nums,i+1,sum+nums[i],tar);
-            solve(nums,i+1,sum-nums[i],tar);
-        }
-        
+            int sum1= solve(nums,i+1,sum+nums[i],tar);
+            int sum2= solve(nums,i+1,sum-nums[i],tar);
            
-        return ans;
+        return sum1+sum2;
     }
-    
-    
+  
     int findTargetSumWays(vector<int>& nums, int target) {
         
         return solve(nums,0,0,target);
