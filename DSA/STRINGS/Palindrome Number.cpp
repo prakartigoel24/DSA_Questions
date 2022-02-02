60e631d10ce22cd3ad3fc03b602e9e6f0ab7317d
Prakarti Goel <prakartigoel24@gmail.com>
2022-02-02 18:36:26 +0530

    Update Palindrome Number.cpp

--- DSA/STRINGS/Palindrome Number.cpp
+++ DSA/STRINGS/Palindrome Number.cpp
@@ -23,5 +23,27 @@ public:
 };
 
 // Method -2
-
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+        int num=x;
+        
+        long long rev=0;
+        
+        if(num<0) return false;
+        
+        while(num!=0)
+        {
+            int rem=num%10;
+            rev= rev * 10 + rem;
+            num/=10;
+        }
+        
+        if(rev==x) return true;
+        
+        return false;
+        
+    }
+};
  
