0760fd216fa877b561a5e7d6e91a564b5302d191
Prakarti Goel <prakartigoel24@gmail.com>
2022-02-03 19:33:48 +0530

    Update Counting bits.cpp

--- DSA/DP/Counting bits.cpp
+++ DSA/DP/Counting bits.cpp
@@ -16,4 +16,21 @@ public:
 };
 
 //Using DP table 
-
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        
+        vector<int> dp(n+1,0);
+        int x=1;
+        for (int i = 1; i <= n; i++)
+        {
+           if(x*2==i)
+               x*=2;
+            
+            dp[i]=1+dp[i-x];
+        
+        }
+        return dp;
+    }
+};
+ 
