b714d7a866b058032d80fa9883cb0a69ecb58a9c
Prakarti Goel <prakartigoel24@gmail.com>
2022-02-03 19:39:18 +0530

    Update Counting bits.cpp

--- DSA/DP/Counting bits.cpp
+++ DSA/DP/Counting bits.cpp
@@ -34,3 +34,19 @@ public:
     }
 };
  
+//USING EVEN ODD PROPERTY OF BINARY NUMBERS
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        
+        vector<int> dp(n+1,0);
+        for (int i = 1; i <= n; i++)
+        {
+            
+            dp[i]=dp[i/2] + i%2;
+        
+        }
+        return dp;
+    }
+};
+ 
