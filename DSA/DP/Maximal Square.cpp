f5f73642fb964f0fc3c88803aa25cbeddcdb8ca7
Prakarti Goel <prakartigoel24@gmail.com>
2022-02-04 14:56:17 +0530

    Update Maximal Square.cpp

--- DSA/DP/Maximal Square.cpp
+++ DSA/DP/Maximal Square.cpp
@@ -1,3 +1,31 @@
 //LINK : https://leetcode.com/problems/maximal-square/
 
 //TABULATION
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& mat) {
+        
+        int r=mat.size();
+        int c=mat[0].size();
+        
+        int dp[r+1][c+1];
+        memset(dp,0,sizeof(dp));
+
+        int maxi=0;
+        for(int i=1;i<=r;i++)
+        {
+            for(int j=1;j<=c;j++)
+            {
+                if(mat[i-1][j-1]=='1')
+                {
+                    dp[i][j]= 1+ min(dp[i-1][j],min(dp[i-1][j-1],dp[i][j-1]));
+                }
+                
+                maxi=max(maxi,dp[i][j]);
+
+            }
+        }
+   
+        return maxi*maxi;
+    }
+};
