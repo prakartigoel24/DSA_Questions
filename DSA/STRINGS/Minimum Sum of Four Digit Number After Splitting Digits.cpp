f67d34923aa2c97c01601b231d9d5deaf4c5244f
Prakarti Goel <prakartigoel24@gmail.com>
2022-02-07 18:27:25 +0530

    Update Minimum Sum of Four Digit Number After Splitting Digits.cpp

--- DSA/STRINGS/Minimum Sum of Four Digit Number After Splitting Digits.cpp
+++ DSA/STRINGS/Minimum Sum of Four Digit Number After Splitting Digits.cpp
@@ -20,3 +20,25 @@ public:
         return a1+a2;
     }
 };
+
+//Sol-2
+class Solution {
+public:
+    int minimumSum(int num) {
+        
+    int arr[4],i=0, new1=0, new2=0;
+        
+        while(num!=0){
+            arr[i]=num%10;
+            num=num/10;
+            i++;
+        }
+        
+        sort(arr, arr+4);
+        new1 = arr[0]*10 +arr[2];
+        new2 = arr[1]*10 +arr[3];
+        return new1 + new2;
+        
+        
+    }
+};
