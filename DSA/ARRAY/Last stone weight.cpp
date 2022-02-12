0cb5db038b5f277ce0ad88b0cb90f02f2c91d517
Prakarti Goel <prakartigoel24@gmail.com>
2022-02-12 12:13:43 +0530

    Update Last stone weight.cpp

--- DSA/ARRAY/Last stone weight.cpp
+++ DSA/ARRAY/Last stone weight.cpp
@@ -39,3 +39,37 @@ public:
     }
 };
 
+//sol-2 (Using max heap)
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        
+         
+        int n =stones.size() ;
+        make_heap(stones.begin(),stones.end());
+        
+        int fe, se;
+        
+        while(n > 1)
+        {
+            fe=stones.front();
+            pop_heap(stones.begin(), stones.end());
+            stones.pop_back();
+            se=stones.front();
+            pop_heap(stones.begin(), stones.end());
+            stones.pop_back();
+            n-=2;
+            
+            if(fe-se > 0)
+            {
+                stones.push_back(fe-se);
+                push_heap(stones.begin(),stones.end());
+                n+=1;
+            }
+          
+        }        
+        if(!stones.empty()) return stones.front();
+        else return 0;
+        
+    }
+};
