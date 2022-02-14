fd7b5d6c54e4827686b5977956ff5405d331125c
Prakarti Goel <prakartigoel24@gmail.com>
2022-02-14 22:58:35 +0530

    Update cherry pickup ii .cpp

--- DSA/DP/cherry pickup ii .cpp
+++ DSA/DP/cherry pickup ii .cpp
@@ -1,5 +1,79 @@
 //LINK : https://leetcode.com/problems/cherry-pickup-ii/
 
+//Optimal Solution
+class Solution {
+public:    
+    int cherryPickup(vector<vector<int>>& grid) {
+    
+        int r =grid.size();
+        int c=grid[0].size();
+        
+        
+        vector<vector<int>> curr(c, vector<int>(c,-1e8));
+        vector<vector<int>> front(c, vector<int>(c,-1e8));
+
+        
+        int maxi=0, maxAns=0;
+       for(int j=0;j<c;j++)
+       {
+           for(int j1=0;j1<c;j1++)
+           {
+               if(j==j1){
+                   front[j][j1]=grid[r-1][j];
+               }
+               else
+                   front[j][j1]=grid[r-1][j]+grid[r-1][j1];
+           }
+       }
+        
+        
+       for(int i=r-2;i>=0;i--)
+       {
+          for(int j1=0;j1<c;j1++)
+          {
+              for(int j2=0;j2<c;j2++)
+              {
+                  maxAns=-1e8;
+                  for(int p1=-1;p1<=1;p1++)
+                  {
+                        for(int p2=-1;p2<=1;p2++)
+                        {
+                            if(j1 == j2)
+                            {
+                                maxi=grid[i][j1];
+                                
+                            }
+                            else
+                            {
+                                maxi=grid[i][j1]+grid[i][j2];
+
+                            }
+                            
+                            
+                            if(j1+p1>=0 and j1+p1 <c and j2+p2>=0 and j2+p2<c)
+                                 maxi+=front[j1+p1][j2+p2];
+                            else
+                                maxi+= -1e8;
+
+                            maxAns=max(maxAns, maxi);
+
+                        }
+
+                  }
+                  
+                  curr[j1][j2]=maxAns;
+              }
+              
+         }
+        
+           front=curr;
+       }
+        
+       return front[0][c-1];
+    }
+};
+
+
 //Memoisation
 
 class Solution {
