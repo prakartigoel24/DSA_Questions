40056d8df72a04322cc3c20f049fc4479e49ae7b
Prakarti Goel <prakartigoel24@gmail.com>
2022-02-17 21:54:29 +0530

    Update Last stone weight-2.cpp

--- DSA/DP/Last stone weight-2.cpp
+++ DSA/DP/Last stone weight-2.cpp
@@ -1,4 +1,4 @@
-//LINK : 
+//LINK : https://leetcode.com/problems/last-stone-weight-ii/
 
 // TABULATION
 //*Variation of subset sum equals target* --> used the last row of dp array to find different subset sums then found the min difference of subsets.
@@ -41,6 +41,54 @@ public:
         for(int i=0;i<=sum/2;i++)
         {
             if(dp[n-1][i]==true)
+            {
+                mini=min(mini,(sum-i)-i);
+            }
+        }
+        
+        return mini;
+    }
+};
+
+//TABULATION - space optimised
+class Solution {
+public:
+ int lastStoneWeightII(vector<int>& nums) {
+       
+       int n = nums.size();        
+        int sum=0;
+        for(auto &it: nums)
+            sum+=it;
+                
+        
+        vector<bool> dp (sum+1,false);
+        vector<bool> prev( sum+1,false);
+        
+        prev[0]=true;
+        dp[0]=true;
+        
+     if(nums[0]<=sum) prev[nums[0]]=true;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<=sum;j++)
+            {
+                bool notTake=prev[j];
+                bool take=false;
+                if(nums[i]<=j)
+                {
+                    take=prev[j-nums[i]];
+                }
+                
+                dp[j]=take|notTake;
+             }
+        
+            prev=dp;
+        }
+        
+        int mini=INT_MAX;
+        for(int i=0;i<=sum/2;i++)
+        {
+            if(prev[i]==true)
             {
                 mini=min(mini,(sum-i)-i);
             }
