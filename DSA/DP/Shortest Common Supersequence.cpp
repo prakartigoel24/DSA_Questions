b4f5ef89b69c6c6b0d9ffb9d69f539840f07381c
Prakarti Goel <prakartigoel24@gmail.com>
2022-02-23 14:32:36 +0530

    Create Shortest Common Supersequence.cpp

---
+++ DSA/DP/Shortest Common Supersequence.cpp
@@ -0,0 +1,71 @@
+// LINK : 
+
+//Tabulation
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        
+        int n=str1.length();
+        int m=str2.length();
+        
+        //finding lcs 
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(str1[i-1]==str2[j-1])
+                {
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+                else
+                {
+                    dp[i][j]= max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        
+        int ii=n, jj=m;
+        
+        string ans="";
+        
+        while(ii>0 and jj>0)
+        {
+            if(str1[ii-1]==str2[jj-1])
+            {
+                ans=str1[ii-1]+ans;
+                ii--,jj--;
+            }
+            else if(dp[ii-1][jj] > dp[ii][jj-1])
+            {
+                 ans=str1[ii-1]+ans;
+                 ii--;
+            }
+            else
+            {
+                ans=str2[jj-1]+ans;        
+                jj--;
+                
+            }
+        }
+        
+        
+        while(ii>0)
+        {
+            ans=str1[ii-1]+ans;
+            ii--;
+        }
+        
+         
+        while(jj>0)
+        {
+            ans=str2[jj-1]+ans;
+            jj--;
+        }
+    
+        return ans;
+      
+    }
+};
