9e604beeb231260bb531127bf7a66df67a0549b8
Prakarti Goel <prakartigoel24@gmail.com>
2022-02-25 16:05:23 +0530

    Update Search in Rotated Sorted Array.cpp

--- DSA/ARRAY/Search in Rotated Sorted Array.cpp
+++ DSA/ARRAY/Search in Rotated Sorted Array.cpp
@@ -13,3 +13,49 @@ public:
     }
 };
 
+//using Modified Binary search
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        
+        int n=nums.size();
+        
+        int s =0 , e=n-1;
+        
+        while(s<=e)
+        {
+            int mid=s+(e-s)/2;
+            
+            if(nums[mid]==target) return mid;
+            
+            else if(nums[s]<=nums[mid])
+            {
+                if(target >=nums[s] and target<= nums[mid])
+                {
+                    e=mid-1;
+                }
+                else 
+                {
+                    s=mid+1;
+                }
+            }
+            else if(nums[e]>=nums[mid])
+            {
+                if(target <=nums[e] and target>= nums[mid])
+                {
+                    s=mid+1;
+                }
+                else
+                {
+                    e=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+
+
+
