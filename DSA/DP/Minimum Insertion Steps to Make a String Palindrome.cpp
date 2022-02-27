45d9d4f119c4111ddb8dd522f60dba12a993ade7
Prakarti Goel <prakartigoel24@gmail.com>
2022-02-27 20:57:30 +0530

    Update Minimum Insertion Steps to Make a String Palindrome.cpp

--- DSA/DP/Minimum Insertion Steps to Make a String Palindrome.cpp
+++ DSA/DP/Minimum Insertion Steps to Make a String Palindrome.cpp
@@ -1,7 +1,40 @@
 // LINK :https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/
 
 //TABULATION [sol-1] (OPTIMISED SOLUTION)
+class Solution {
+public:
+    int minInsertions(string s) {
+        
+        int n=s.length();
+        string rev=s;
+        reverse(rev.begin(),rev.end());
+        
+        vector<int> curr(n+1,0), prev(n+1,0);
+        
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(s[i-1]==rev[j-1])
+                {
+                    curr[j]=1+prev[j-1];
+                }
+                else
+                {
+                    curr[j]=max(prev[j],curr[j-1]);
+                }
+            }
+            
+            prev=curr;
+        }
+        
+        return n-curr[n];
+        
+    }
+};
 
+//TABULATION [sol-2] 
 class Solution {
 public:
     int minInsertions(string s) {
