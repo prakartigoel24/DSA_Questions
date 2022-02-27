f93b6bdfb297b33ba37b2c75be4c68050df00112
Prakarti Goel <prakartigoel24@gmail.com>
2022-02-27 21:01:28 +0530

    Update Longest Palindromic Subsequence.cpp

--- DSA/DP/Longest Palindromic Subsequence.cpp
+++ DSA/DP/Longest Palindromic Subsequence.cpp
@@ -1,7 +1,40 @@
 //LINK : https://leetcode.com/problems/longest-palindromic-subsequence/
 
-//TABULATION [sol-1]
+//TABULATION [sol-1] (OPTIMISED SOLUTION)
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+         
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+        
+        int n =s.length();
+        vector<int> curr(n+1,0), prev(n+1,0);
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(s[i-1]==rev[j-1])
+                {
+                    curr[j]=1+prev[j-1];
+                }
+                else
+                {
+                    curr[j]=max(prev[j],curr[j-1]);
+                }
+            }
+            
+            prev=curr;
+        }
+        
+       
+        return curr[n]; 
+        
+    }
+};
 
+//TABULATION [Sol-2]
 class Solution {
 public:
     int longestPalindromeSubseq(string s) {
