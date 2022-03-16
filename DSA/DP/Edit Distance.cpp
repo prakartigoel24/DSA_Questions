40ee2ba95cde1b6adbae5a526d0eab14ff0c6a70
Prakarti Goel <prakartigoel24@gmail.com>
2022-03-16 19:13:47 +0530

    Update Edit Distance.cpp

--- DSA/DP/Edit Distance.cpp
+++ DSA/DP/Edit Distance.cpp
@@ -1,5 +1,46 @@
 // LINK : https://leetcode.com/problems/edit-distance/
 
+//Tabulation - optimised
+class Solution {
+public:
+    int minDistance(string s1, string s2) {
+        
+        int n = s1.size();
+        int m=s2.size();
+        
+        vector<int> prev (m+1,0), curr(m+1,0);
+        
+        for(int j=0;j<=m;j++)
+        {
+            prev[j]=j;
+        }
+    
+        for(int i=1;i<=n;i++)
+        {
+            curr[0]=i;
+            for(int j=1;j<=m;j++)
+            {
+                int replace=0, del=0, insert =0;
+                if(s1[i-1]==s2[j-1])
+                {
+                   curr[j]= prev[j-1];
+                }
+                else
+                {
+                    replace  = 1 + prev[j-1];
+                    del = 1 + prev[j];
+                    insert = 1 + curr[j-1];
+                    curr[j]= min({replace, del, insert});
+
+                }
+            }
+            prev=curr;   
+        }
+        return prev[m];  
+    }
+};
+
+
 //Tabulation
 class Solution {
 public:
