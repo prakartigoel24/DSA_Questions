798199ad55dcb1213a42be01831387ec1c55000c
Prakarti Goel <prakartigoel24@gmail.com>
2022-03-16 18:50:08 +0530

    Update Distinct Subsequences.cpp

--- DSA/DP/Distinct Subsequences.cpp
+++ DSA/DP/Distinct Subsequences.cpp
@@ -1,5 +1,85 @@
 //LINK : https://leetcode.com/problems/distinct-subsequences/
 
+//Tabulation - optimised
+class Solution {
+public:
+
+    int numDistinct(string s, string t) {
+        int n =s.size();
+        int m=t.size();
+        vector<double> curr(m+1,0) , prev(m+1,0) ; 
+       
+        curr[0]=1;
+        prev[0]=1;
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                double take=0 , nottake=0;
+                if(s[i-1]==t[j-1])
+                {
+                    take=prev[j-1];
+                    nottake = prev[j];
+                     curr[j]= take+nottake;
+                }
+               else
+               {
+                     curr[j]= prev[j];
+               }
+            }
+            
+            prev=curr;
+        }
+        
+        
+        return (int)prev[m];
+    }
+};
+
+
+//Tabulation -2
+class Solution {
+public:
+
+    int numDistinct(string s, string t) {
+        int n =s.size();
+        int m=t.size();
+        vector<vector<double>> dp(n+1,vector<double> (m+1,-1));
+       
+        for(int j=1;j<=m;j++)
+        {
+            dp[0][j] = 0;
+        }
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0] = 1;
+        }
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                double take=0 , nottake=0;
+                if(s[i-1]==t[j-1])
+                {
+                    take=dp[i-1][j-1];
+                    nottake = dp[i-1][j];
+                     dp[i][j]= take+nottake;
+                }
+               else
+               {
+                     dp[i][j]= dp[i-1][j];
+               }
+            }
+        }
+        
+        
+        return (int)dp[n][m];
+    }
+};
+
+
 //Memoisation 
 class Solution {
 public:
