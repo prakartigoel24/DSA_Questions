39e0151324b607cfc43e0a1b8deb1334af969e3b
Prakarti Goel <prakartigoel24@gmail.com>
2022-03-16 19:03:00 +0530

    Update Edit Distance.cpp

--- DSA/DP/Edit Distance.cpp
+++ DSA/DP/Edit Distance.cpp
@@ -5,19 +5,19 @@ class Solution {
 public:
     int f(string s1, string s2, int i, int j, vector<vector<int>> &dp)
     {
-        if(i<0)
+        if(i==0)
         {
-            return j+1;
+            return j;
         }
-        if(j<0)
+        if(j==0)
         {
-            return i+1;
+            return i;
         }
         
         if(dp[i][j]!=-1) return dp[i][j];
         
         int replace=0, del=0, insert =0;
-        if(s1[i]==s2[j])
+        if(s1[i-1]==s2[j-1])
         {
             return dp[i][j]= f(s1, s2, i-1,j-1,dp);
         }
@@ -34,14 +34,13 @@ public:
         
     }
     int minDistance(string s1, string s2) {
-        
-        
+  
         int n = s1.size();
         int m=s2.size();
         
         vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
         
-        return f(s1, s2,n-1,m-1,dp);
+        return f(s1, s2,n,m,dp);
         
         
         
