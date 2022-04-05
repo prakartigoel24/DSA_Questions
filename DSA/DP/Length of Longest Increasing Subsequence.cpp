3012de668be775417d626560a5d4e4de86fc9b98
Prakarti Goel <prakartigoel24@gmail.com>
2022-04-05 21:54:46 +0530

    Update Length of Longest Increasing Subsequence.cpp

--- DSA/DP/Length of Longest Increasing Subsequence.cpp
+++ DSA/DP/Length of Longest Increasing Subsequence.cpp
@@ -1,30 +1,87 @@
 //LINK : https://leetcode.com/problems/longest-increasing-subsequence/ 
 
+//OPTIMISED APPROACH 
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& arr) {
+        
+        int n=arr.size(), maxi=1;
+        vector<int> dp(n,1);
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int prev=0;prev<i;prev++)
+            {
+                if(arr[i] > arr[prev])
+                {
+                    dp[i]=max(dp[i], 1+dp[prev]);
+                }
+            }
+            
+            maxi=max(maxi,dp[i]);
+        }
+        
+        return maxi;
+    }
+};
+
 //TABULATION [Sol-1]
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
+    int lengthOfLIS(vector<int>& arr) {
         
-        int n= nums.size();
-        vector<int> dp (n,INT_MIN);
-        dp[0]=1;
+        int n=arr.size();
+        vector<vector<int>> dp(n+1,vector<int> (n+1,0));
         
-        int maxi=0 , ans=1;
-        for(int i=1;i<n;i++)
+        for(int i=n-1;i>=0;i--)
         {
-             maxi=0;
-            for(int j=0;j<i;j++)
+            for(int prev=i-1;prev>=-1;prev--)
             {
-                if(nums[i] > nums[j])
+                int take=0 , nottake=0;
+
+                nottake = 0 + dp[i+1][prev+1];
+
+                if( prev == -1 || arr[i] > arr[prev] )
                 {
-                    maxi=max(maxi,dp[j]);
+                    take = 1 +dp[i+1][i+1];
                 }
+
+                 dp[i][prev+1]=max(take,nottake);
+
             }
-            dp[i]=maxi+1;
-            
-           ans= max(ans, dp[i]);
         }
-    
-        return ans;
+        
+        
+        return dp[0][0];
+    }
+};
+
+//TABULATION [SOL-2]
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& arr) {
+        
+        int n=arr.size();
+        vector<int> curr(n+1,0), next(n+1,0);
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int prev=i-1;prev>=-1;prev--)
+            {
+                int take=0 , nottake=0;
+
+                nottake = 0 + next[prev+1];
+
+                if( prev == -1 || arr[i] > arr[prev] )
+                {
+                    take = 1 +next[i+1];
+                }
+
+                 curr[prev+1]=max(take,nottake);
+
+            }
+            next=curr;
+        }
+        return curr[0];
     }
 };
