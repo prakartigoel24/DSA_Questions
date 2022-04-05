11b229265735997a860316ea326af041faeea286
Prakarti Goel <prakartigoel24@gmail.com>
2022-04-05 19:37:40 +0530

    Update buy-and-sell-stock-II.cpp

--- DSA/DP/buy-and-sell-stock-II.cpp
+++ DSA/DP/buy-and-sell-stock-II.cpp
@@ -1,6 +1,111 @@
 //LINK : https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
 
-//Approach -1
+//Most optimised approach 
+//Tabulation
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int n=prices.size();
+        
+      vector<int> next(2,0), cur(2,0);
+        
+        next[0]=next[1]=0;
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=0;j<=1;j++)
+            {
+                long long profit=0;
+                if(j)
+                {
+                    cur[j] = max(-prices[i]+next[0] , next[1]);
+                }
+                else
+                {
+                    cur[j] = max(prices[i]+next[1],next[0]);
+                }
+            }
+            next=cur;
+        }
+        
+       return next[1];
+    }
+};
+
+//Approach-1 (Tabulation)
+
+//Tabulation
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int n=prices.size();
+        
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        
+        dp[n][0]=dp[n][1]=0;
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=0;j<=1;j++)
+            {
+                long long profit=0;
+                if(j)
+                {
+                    dp[i][j] = max(-prices[i]+dp[i+1][0] , dp[i+1][1]);
+                }
+                else
+                {
+                    dp[i][j] = max(prices[i]+dp[i+1][1],dp[i+1][0]);
+                }
+            }
+        }
+        
+       return dp[0][1];
+    }
+};
+
+
+
+
+//Approach-2 (memoisation)
+
+class Solution {
+public:
+    int maxPro(vector<int>& prices, int i, int canbuy , vector<vector<int>> &dp)
+    {
+        int n=prices.size();
+        
+        if(i==n)
+            return 0;
+        
+        if(dp[i][canbuy]!=-1) return dp[i][canbuy];
+        long long profit=0;
+        if(canbuy)
+        {
+            profit = max(-prices[i]+maxPro(prices,i+1,0, dp) ,maxPro(prices,i+1,1,dp));
+        }
+        else
+        {
+            profit = max(prices[i]+maxPro(prices,i+1,1,dp), maxPro(prices, i+1,0,dp));
+        }
+        
+        return dp[i][canbuy] = profit;        
+    }
+    
+    int maxProfit(vector<int>& prices) {
+        
+        int n=prices.size();
+        
+        vector<vector<int>> dp(n,vector<int>(2,-1));
+        
+        return maxPro(prices,0,1, dp);
+    }
+};
+
+
+//Approach -3
 class Solution {
 public:
     int maxProfit(vector<int>& arr) {
@@ -25,21 +130,3 @@ public:
     }
 };
 
-//Approach-2
-
-class Solution {
-public:
-    int maxProfit(vector<int>& arr) {
-        
-        int totProfit=0;
-        for(int i=0;i<arr.size()-1;i++)
-        {
-            if(arr[i]< arr[i+1])
-            totProfit+=(arr[i+1]-arr[i]);
-        
-        }
-        
-        return totProfit;
-        
-    }
-};
