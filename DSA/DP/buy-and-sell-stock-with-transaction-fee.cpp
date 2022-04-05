73b7db86d3e8703633f53b3c44193d1d301c821f
Prakarti Goel <prakartigoel24@gmail.com>
2022-04-05 21:09:45 +0530

    Update buy-and-sell-stock-with-transaction-fee.cpp

--- DSA/DP/buy-and-sell-stock-with-transaction-fee.cpp
+++ DSA/DP/buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,62 +1,123 @@
 //LINK : https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
 
-//using MEMOIZATION
-
+//OPTIMISED SOLUTION
 class Solution {
-    int dp[50001][2];
 public:
-    
-int maxProfitWithFee(int i,bool own,int fee, vector<int> &prices)
-{
-  if(i==prices.size())
-    return 0;
-
-  if(dp[i][own]!=-1) return dp[i][own];
-    
-  if(own)
-  {
-      int op1= prices[i] + maxProfitWithFee(i+1,0,fee,prices);
-      int op2= maxProfitWithFee(i+1,own,fee,prices);
-      return dp[i][own]=max(op1,op2);
-  }
-  else
-  {
-      int op1= -(prices[i]+fee) + maxProfitWithFee(i+1,1,fee,prices);;
-      int op2=  maxProfitWithFee(i+1,0,fee,prices);;
-      return dp[i][own]=max(op1,op2);
-  }
-
-}
-    
-int maxProfit(vector<int>& arr, int fee) {
-          memset(dp,-1,sizeof(dp));
-          
-          return maxProfitWithFee(0,0,fee,arr);
+    int maxProfit(vector<int>& prices, int fee) {
+      
+        int n=prices.size();
+        
+      vector<int> next(2,0), cur(2,0);
+        
+        next[0]=next[1]=0;
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=0;j<=1;j++)
+            {
+                long long profit=0;
+                if(j)
+                {
+                    cur[j] = max(-prices[i]+next[0] , next[1]);
+                }
+                else
+                {
+                    cur[j] = max(prices[i]-fee+next[1],next[0]);
+                }
+            }
+            next=cur;
+        }
+        
+       return next[1];
     }
 };
 
-
-//USING TABULATION
-
+//optimised sol-2 
 class Solution {
-    int dp[50001][2];
-    public:
+public:
     int maxProfit(vector<int>& prices, int fee) {
-              memset(dp,-1,sizeof(dp));
-
-            int n = prices.size();
-            if(n<=1) return 0;
-
-            dp[0][0]=0;
-            dp[0][1]= -(prices[0]+fee);
-
-            for(int i=1;i<n;i++)
-            {
-                dp[i][1]=max(-(prices[i]+fee) + dp[i-1][0]  ,dp[i-1][1] );
-                dp[i][0]=max( prices[i] + dp[i-1][1] , dp[i-1][0]);
-
-            }
-
-             return dp[n-1][0];
+        
+     
+        int n=prices.size();
+        
+        vector<int> next(2,0), cur(2,0);
+        
+        next[0]=next[1]=0;
+        
+        for(int i=n-1;i>=0;i--)
+        {
+                        
+            cur[1] = max(-prices[i]+next[0] , next[1]);
+               
+            cur[0] = max(prices[i]-fee+next[1],next[0]);
+            
+            next=cur;
         }
-    };
+        
+       return next[1];
+    }
+};
+
+//TABULATION
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        
+        int n=prices.size();
+        
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        
+        dp[n][0]=dp[n][1]=0;
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=0;j<=1;j++)
+            {
+                long long profit=0;
+                if(j)
+                {
+                    dp[i][j] = max(-prices[i]+dp[i+1][0] , dp[i+1][1]);
+                }
+                else
+                {
+                    dp[i][j] = max(prices[i]-fee+dp[i+1][1],dp[i+1][0]);
+                }
+            }
+        }
+        
+       return dp[0][1];
+        
+    }
+};
+
+//MEMOISATION
+class Solution {
+public:
+    int maxPro(vector<int>& prices, int i, int canbuy ,int fee, vector<vector<int>> &dp)
+    {
+        int n=prices.size();
+        
+        if(i==n)
+            return 0;
+        
+        if(dp[i][canbuy]!=-1) return dp[i][canbuy];
+        long long profit=0;
+        if(canbuy)
+        {
+            profit = max(-prices[i]+maxPro(prices,i+1,0,fee, dp) ,maxPro(prices,i+1,1,fee,dp));
+        }
+        else
+        {
+            profit = max(prices[i]-fee+maxPro(prices,i+1,1,fee,dp), maxPro(prices, i+1,0,fee,dp));
+        }
+        
+        return dp[i][canbuy] = profit;        
+    }
+    
+    int maxProfit(vector<int>& prices, int fee) {
+
+        int n=prices.size();
+        vector<vector<int>> dp(n,vector<int>(2,-1));
+        return maxPro(prices,0,1,fee,dp);
+    }
+};
