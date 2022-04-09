c6633039cbd5162a6040b5f21e6bf2b3bb8559f5
Prakarti Goel <prakartigoel24@gmail.com>
2022-04-09 14:16:41 +0530

    Update Wildcard Matching.cpp

--- DSA/DP/Wildcard Matching.cpp
+++ DSA/DP/Wildcard Matching.cpp
@@ -1,5 +1,107 @@
 #LINK : https://leetcode.com/problems/wildcard-matching/
 
+//Space optimised -Tabulation
+class Solution {
+public: 
+    bool isMatch(string s, string p) {
+
+        int n=s.length(), m=p.length();
+        vector<bool> curr(m+1,false), next(m+1,false);
+        
+        next[m]=true; //base case
+        
+        for(int j=0;j<m;j++) //base case
+        {
+            bool flag=true;
+            for(int k=j;k<m;k++)
+            {
+                if(p[k]!='*')
+                {
+                    flag=false;
+                    break;
+                }
+                   
+            }
+            next[j]=flag;
+        }
+        
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=m-1;j>=0;j--)
+            {
+                if(s[i]==p[j] or p[j]=='?')
+                {
+                    curr[j]= next[j+1];
+                }
+                else if(p[j]=='*')
+                {
+                   curr[j]= next[j] or curr[j+1];
+                }
+                else
+                {
+                    curr[j]= false;
+                }
+            }
+            next=curr;
+        }
+        
+        return next[0];
+    }
+};
+
+
+//Tabulation
+class Solution {
+public: 
+    bool isMatch(string s, string p) {
+
+        int n=s.length(), m=p.length();
+        vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
+        
+        dp[n][m]=true; //base case
+        
+        for(int j=0;j<m;j++) //base case
+        {
+            bool flag=true;
+            for(int k=j;k<m;k++)
+            {
+                if(p[k]!='*')
+                {
+                    flag=false;
+                    break;
+                }
+                   
+            }
+            dp[n][j]=flag;
+        }
+        
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=m-1;j>=0;j--)
+            {
+                if(s[i]==p[j] or p[j]=='?')
+                {
+                    dp[i][j]= dp[i+1][j+1];
+                }
+                else if(p[j]=='*')
+                {
+                   dp[i][j]= dp[i+1][j] or dp[i][j+1];
+                }
+                else
+                {
+                    dp[i][j]= false;
+                }
+
+            }
+        }
+        
+        return dp[0][0];
+    }
+};
+
+
 //Memoisation solution
 class Solution {
 public:
