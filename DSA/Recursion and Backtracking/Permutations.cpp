1f1fe61e5989b9c9884c567bd79010cc9f6c207c
Prakarti Goel <prakartigoel24@gmail.com>
2022-08-19 19:43:57 +0530

    Update Permutations.cpp

--- DSA/Recursion and Backtracking/Permutations.cpp
+++ DSA/Recursion and Backtracking/Permutations.cpp
@@ -1,5 +1,45 @@
 // LINK : https://leetcode.com/problems/permutations/
 
+//solution 1
+class Solution {
+public:
+    vector<vector<int>> storePermutes(vector<int>& nums, int i) {
+        
+        if(i == nums.size()-1)
+        {
+            vector<vector<int>>newP;
+            newP.push_back({nums[i]});
+            return newP;
+        }
+        
+        int firstEle = nums[i];  // first element
+        vector<vector<int>> perms = storePermutes(nums,i+1); // permutations from i+1 index
+        vector<vector<int>>newPerms;
+
+        for(int ii =0 ;ii < perms.size(); ii++)
+        {
+            for(int j = 0; j<= perms[0].size();j++)
+            {
+                vector<int> temp = perms[ii];
+                temp.insert(temp.begin()+j,firstEle); // first element inserted in between all permutations.
+                newPerms.push_back(temp);
+            }
+            
+        }
+        
+        return newPerms;
+        
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+      
+        vector<vector<int>> ans = storePermutes(nums, 0);
+        return ans;
+        
+    }
+};
+
+//Solution using swapping 
 class Solution {
 public:
     void solve(vector<int>& nums, vector<vector<int>> &ans, int ind )
