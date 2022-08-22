a0a1898a1c673bed97a5fc780ca734bbb38cb4da
Prakarti Goel <prakartigoel24@gmail.com>
2022-08-22 13:04:01 +0530

    Create Merge Intervals.cpp

---
+++ DSA/ARRAY/Merge Intervals.cpp
@@ -0,0 +1,29 @@
+// LINK : https://leetcode.com/problems/merge-intervals/
+
+//  Using new Merged vector
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        sort(intervals.begin(),intervals.end());
+        int n = intervals.size(), s=1;
+        
+        vector<vector<int>> merged;
+        merged.push_back(intervals[0]);
+        
+        for(int i=1;i<n;i++)
+        {
+            if(intervals[i][0]<=merged[s-1][1])
+            {
+                merged[s-1][1]= (intervals[i][1] > merged[s-1][1]?intervals[i][1]:merged[s-1][1]);
+            }
+            else
+            {
+                merged.push_back(intervals[i]);
+                s++;
+            }
+        }
+        
+        return merged;
+    }
+};
