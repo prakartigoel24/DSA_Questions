8918626d397777efb709c52204bf21afd1385d7f
Prakarti Goel <prakartigoel24@gmail.com>
2022-08-23 21:08:30 +0530

    Update Majority Element (  > N by 3 ).cpp

--- DSA/ARRAY/Majority Element (  > N by 3 ).cpp
+++ DSA/ARRAY/Majority Element (  > N by 3 ).cpp
@@ -1,6 +1,62 @@
 // LINK : https://leetcode.com/problems/majority-element-ii/
 
 //Optimal - Extended Moore's voting algo
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<int> ans;
+        int num1= -1, num2 = -1, cnt1=0, cnt2=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==num1)
+            {
+                cnt1++;
+            }
+            else if(nums[i]==num2)
+            {
+                cnt2++;
+            }
+            else if(cnt1==0)
+            {
+                num1= nums[i];
+                cnt1=1;
+            }
+            else if(cnt2==0)
+            {
+                num2= nums[i];
+                cnt2=1;
+            }
+            else
+            {
+                cnt1--, cnt2--;
+            }
+            
+        }
+        cnt1=cnt2=0;
+        for(auto it:nums)
+        {
+            if(it==num1)
+            {
+                cnt1++;
+            }
+            
+            if(it==num2)
+            {
+                cnt2++;
+            }
+            
+        }
+        
+        if(cnt1 > n/3) ans.push_back(num1);
+        if(cnt2 > n/3) ans.push_back(num2);
+        if(num1==num2) ans.pop_back();
+        return ans;
+    }
+};
+
 
 //Brute force  
 class Solution {
