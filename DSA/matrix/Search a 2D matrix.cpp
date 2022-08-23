d160caa0be8d8caed6b29c543d0fa6db29210eeb
Prakarti Goel <prakartigoel24@gmail.com>
2022-08-23 19:05:59 +0530

    Update Search a 2D matrix.cpp

--- DSA/matrix/Search a 2D matrix.cpp
+++ DSA/matrix/Search a 2D matrix.cpp
@@ -1,6 +1,61 @@
 // LINK : https://leetcode.com/problems/search-a-2d-matrix/
 
-//Approach -1  [TC - O(N*logM)]
+//Approach -1 [TC - O(log(m*n) ]
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+        
+        int row = mat.size();
+        int col = mat[0].size();
+           
+        int lo=0; int hi = row*col - 1;
+        
+        while(lo<=hi)
+        {
+            int mid = lo + (hi-lo)/2;
+            int r = mid/col;
+            int c = mid%col;
+            
+            if(mat[r][c]==target)
+            {
+                return true;
+            }
+            else if(mat[r][c] < target)
+            {
+                lo=mid+1;
+            }
+            else
+                hi = mid-1;
+        }
+        
+        return false;
+    }
+};
+
+//Approach -2 [TC - O(N+M)]
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+        
+        int row = mat.size();
+        int col = mat[0].size();
+        int i = 0 , j = col-1;
+        
+        while(j>=0 and i<row)
+        {
+            if(mat[i][j]==target) return true;
+            else if(mat[i][j] < target)
+            {
+                i++;
+            }
+            else
+                j--;
+        }
+        return false;
+    }
+};
+
+//Approach -3  [TC - O(N*logM)]
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& mat, int target) {
